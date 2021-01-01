80d6e7bcdba94e49b63cc3d2f7ef74590c57af7c
feernando <fernandofitilan@hotmail.com>
2021-01-01 13:17:22 -0300

    up

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Suzana',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "Teste",
+  },
+  {
+    id: 2,
+    name: 'Marilda',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Teste.',
+  },
+  {
+    id: 3,
+    name: 'Paulinho',
+    job: 'Web full stack',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Teste.',
+  },
+  {
+    id: 4,
+    name: 'Gilson',
+    job: 'gestor',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Gestor completo!!! ',
+  },
+];
+
+export default reviews;
